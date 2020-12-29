dfa570bf1bbc958caf0b9748779b57852c196162
Nick Tolley <nic.m.tolley@gmail.com>
2020-12-29 16:25:49 -0500

    [DATALAD] Recorded changes

--- code/beta/run_hnn.py
+++ code/beta/run_hnn.py
@@ -12,21 +12,22 @@ import multiprocessing
 import datetime
 import dill
 import joblib
+from joblib import Parallel, delayed
 from dask.distributed import Client
 time_stamp = datetime.datetime.now().strftime("%m%d%Y_%H%M%S")
 
 client = Client(processes=False)
 
 save_suffix = 'beta_event' + '_' + time_stamp
-save_path = '../data/beta/prerun_simulations/' + time_stamp + '/'
+save_path = '../../data/beta/prerun_simulations/' + time_stamp + '/'
 os.mkdir(save_path)
 
-params_fname = '../data/beta/params/beta_param.param
+params_fname = '../../data/beta/params/beta_param.param'
 
 num_simulations = 10
 
-param_dict = {'dipole_scalefctr': (60000, 200000),
- 't_evprox_1': 225, 255),
+prior_dict = {'dipole_scalefctr': (60000, 200000),
+ 't_evprox_1': (225, 255),
  'sigma_t_evprox_1': (10, 50),
  'numspikes_evprox_1': (1, 20),
  'gbar_evprox_1_L2Pyr_ampa': (1e-06, 0.0005),
@@ -44,7 +45,7 @@ prior = utils.BoxUniform(low=torch.tensor(param_low), high=torch.tensor(param_hi
 theta_samples = prior.sample_n(num_simulations)
 
 def dill_save(save_object, save_prefix, save_suffix, save_path, extension='.pkl'):
-    save_file = open(save_path + save_prefix + '_' + save_suffix, 'wb')
+    save_file = open(save_path + save_prefix + '_' + save_suffix + extension, 'wb')
     dill.dump(save_object, save_file)
     save_file.close()
 
@@ -55,6 +56,7 @@ dill_save(theta_samples, 'theta_samples', save_suffix, save_path)
 class HNNSimulator:
     def __init__(self, params_fname, prior_dict):
         self.params = read_params(params_fname)
+        self.params['tstop'] = 30
         self.param_names = list(prior_dict.keys())
 
     def __call__(self, new_param_values):
@@ -70,17 +72,19 @@ class HNNSimulator:
 
 
 hnn_simulator = HNNSimulator(params_fname,prior_dict)
+sbi_simulator, sbi_prior = prepare_for_sbi(hnn_simulator, prior)
 
-for sim_idx in range(num_simulations):
-    new_param_values = theta_samples[sim_idx,:]
-    dpl = hnn_simulator(new_param_values)
+# def run_simulator(simulator, theta, sim_idx):
+#     new_param_values = theta_samples[sim_idx,:]
+#     dpl = simulator(new_param_values)
 
-    dpl_name = save_path + save_prefix + '_' + save_suffix + 'dpl_sim{}'.format(sim_idx) + '.txt'
-    param_name = save_path + save_prefix + '_' + save_suffix + 'theta_sim{}'.format(sim_idx) + '.txt'
+#     dpl_name = save_path + save_prefix + '_' + save_suffix + 'dpl_sim{}'.format(sim_idx) + '.txt'
+#     param_name = save_path + save_prefix + '_' + save_suffix + 'theta_sim{}'.format(sim_idx) + '.txt'
 
-    np.savetxt(dpl_name, dpl, delimiter=',')
-    np.savetxt(param_name, new_param_values, delimiter=',')
+#     np.savetxt(dpl_name, dpl, delimiter=',')
+#     np.savetxt(param_name, new_param_values, delimiter=',')
 
-    
-
+with joblib.parallel_backend('dask'):    
+        # Parallel(verbose=100,n_jobs=12)(delayed(run_simulator)(sbi_simulator, theta_samples[sim_idx,:], sim_idx) for sim_idx in range(num_simulations))
+        theta, x = simulate_for_sbi(sbi_simulator, proposal=sbi_prior, num_simulations=num_simulations, num_workers=12)
 
