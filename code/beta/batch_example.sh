f4fd3ab35b8401dfb42c906b45a142116c16ec16
Nick Tolley <nic.m.tolley@gmail.com>
2021-02-25 09:26:43 -0500

    Clean up slurm output

--- code/beta/batch_example.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#SBATCH --mem=20g --ntasks-per-node=16 -N 1
-#SBATCH -t 1:00:00
-#SBATCH -A carney-sjones-condo
-
-/users/ntolley/anaconda/sbi/bin/python /users/ntolley/Jones_Lab/sbi_hnn_github/code/beta/python_example.py
